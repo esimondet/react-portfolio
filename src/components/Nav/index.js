cf5347d5ee4d4f4610c26e6efb8f4982512cf42f
Edison Simondet <edison.simondet@gmail.com>
2022-02-26 15:03:42 -0600

    initial bulma integration

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,10 +2,29 @@ import React from 'react';
 
 function Nav() {
   return (
-    <div>
-      <nav> Menu </nav>
-      <h1> Edison Simondet </h1>
-    </div>
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <h1>Edison Simondet</h1>
+      </div>
+      {/* probably have to do a hook here with aria-expanded */}
+      <div
+        role="button"
+        className="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </div>
+      <div id="navbarBasic" className="navbar-menu">
+        <div className="navbar-start">
+          <a className="navbar-item projectsBtn">Projects</a>
+          <a className="navbar-item aboutBtn">About Me</a>
+          <a className="navbar-item contactBtn">Contact</a>
+        </div>
+      </div>
+    </nav>
   );
 }
 
